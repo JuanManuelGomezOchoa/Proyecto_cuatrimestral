f678b6047acbf09eb8dd2107a6e86d8005547169
JuanManuelGomezOchoa <utm23090605@utma.edu.mx>
2024-11-25 14:48:09 -0600

    Commit de la clase del 25 de Noviembre

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { UserModel } from "../models/UsersModel";
+import jwt from "jsonwebtoken"
 
 export const registerUsers = async (req:Request, res:Response):Promise<any> =>{
     try {
@@ -27,14 +28,17 @@ export const registerUsers = async (req:Request, res:Response):Promise<any> =>{
             })
         } 
 
-        await UserModel.create({
+        const user = await UserModel.create({
             name,
             lastNames,
             email,
             password,
             rol       
         })
-        return res.status(200).json({msg:"Usuarios registrados con exito"})
+
+        const token = jwt.sign(JSON.stringify(user), "shhh");
+
+        return res.status(200).json({msg:"Usuarios registrados con exito", token})
     } catch (error) {
         console.log(error);
         return res.status(500).json({msg:"Hubo un error al crear los usuarios"})
