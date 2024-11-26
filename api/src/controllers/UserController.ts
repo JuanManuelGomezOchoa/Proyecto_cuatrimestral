84507d1d94141a78769573dd2c307c879b725736
JuanManuelGomezOchoa <utm23090605@utma.edu.mx>
2024-11-26 14:23:30 -0600

    commit con la tarea

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -44,3 +44,30 @@ export const registerUsers = async (req:Request, res:Response):Promise<any> =>{
         return res.status(500).json({msg:"Hubo un error al crear los usuarios"})
     }
 }
+
+export const singIn = async (req: Request, res: Response): Promise<any> => {
+    try {
+        //Correo y contraseña
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ msg: "El campo de correo y contraseña son requeridos" });
+        }
+
+        // Verificar que el usuario existe con el email y la passwor
+        const user = await UserModel.findOne({email:req.body.email, password:req.body.password});
+
+        // Si no existe devuelve un error
+        if (!user) {
+            return res.status(404).json({ msg: "El usuario no existe" });
+        }
+
+        // Devolver un token
+        const token = jwt.sign(JSON.stringify(user), "shhh");
+
+        return res.status(200).json({ msg: "El usuario si existe", token });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msg: "Hubo un error al intentar iniciar sesion" });
+    }
+};
